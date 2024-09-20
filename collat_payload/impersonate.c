02bc3ef8dd9a2ec5766706045f2b9baa8c365e7c
tuxuser <462620+tuxuser@users.noreply.github.com>
2024-09-20 02:23:35 +0200

    simplify & correct impersonation a bit

--- collat_payload/impersonate.c
+++ collat_payload/impersonate.c
@@ -9,44 +9,13 @@ CHAR* global_cur_msg[0x200] = { 0 };
 	sprintf(global_cur_msg, x, __VA_ARGS__); \
 	send(winSock, global_cur_msg, strlen(global_cur_msg), 0);
 
-BOOL IsPrivilegeEnabled(LPWSTR privilege)
-{
-    BOOL ret;
-	LUID luid = { 0 };
-    HANDLE hProcess = GetCurrentProcess();
-    HANDLE hToken = INVALID_HANDLE_VALUE;
-    if (hProcess == INVALID_HANDLE_VALUE)
-        return FALSE;
-
-    if (!OpenProcessToken(hProcess, TOKEN_QUERY, &hToken)){
-		LOG_SOCK("%s:%d: Failed to open process token\n", __FUNCTION__, __LINE__);
-        return FALSE;
-	}
-
-	if (!LookupPrivilegeValue(NULL, privilege, &luid)) {
-		LOG_SOCK("%s:%d: Failed to lookup privilege value\n", __FUNCTION__, __LINE__);
-		return FALSE;
-	}
-
-    PRIVILEGE_SET privs = { .Privilege = {0}, .Control = PRIVILEGE_SET_ALL_NECESSARY, .PrivilegeCount = 1};
-    privs.Privilege[0].Luid = luid;
-    privs.Privilege[0].Attributes = SE_PRIVILEGE_ENABLED;
-
-    if (!PrivilegeCheck(hToken, &privs, &ret)) {
-		LOG_SOCK("%s:%d: Failed privilege check\n", __FUNCTION__, __LINE__);
-        return FALSE;
-	}
-
-    return ret;
-}
-
 BOOL EnablePrivilege(LPWSTR Privilege)
 {
 	LUID luid = { 0 };
     HANDLE hProcess = GetCurrentProcess();
     HANDLE hToken;
 
-	if (!OpenProcessToken(hProcess, TOKEN_QUERY | TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+	if (!OpenProcessToken(hProcess, TOKEN_ALL_ACCESS, &hToken)) {
 		LOG_SOCK("%s:%d: Failed to open process token\n", __FUNCTION__, __LINE__);
 		return FALSE;
 	}
@@ -61,13 +30,21 @@ BOOL EnablePrivilege(LPWSTR Privilege)
     // Now we create a TOKEN_PRIVILEGES structure with our modifications
 	TOKEN_PRIVILEGES tp = { .PrivilegeCount = 1, .Privileges = {0} };
     tp.Privileges[0] = luAttr;
-	TOKEN_PRIVILEGES oldState = {0}; // Our old state.
-	int returnLength = 0;
 
-	if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), &oldState, &returnLength)) {
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tp, 0, NULL, NULL)) {
 		LOG_SOCK("%s:%d: Failed to adjust privs\n", __FUNCTION__, __LINE__);
 		return FALSE;
 	}
+
+    BOOL ret;
+    PRIVILEGE_SET privs = { .Privilege = {0}, .Control = PRIVILEGE_SET_ALL_NECESSARY, .PrivilegeCount = 1 };
+    privs.Privilege[0].Luid = luid;
+    privs.Privilege[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    if (!PrivilegeCheck(hToken, &privs, &ret)) {
+        LOG_SOCK("%s:%d: Failed privilege check\n", __FUNCTION__, __LINE__);
+        return FALSE;
+    }
 
     return TRUE;
 }
@@ -81,13 +58,13 @@ BOOL ImpersonateProcessToken(int pid)
 	}
 
 	HANDLE hToken = INVALID_HANDLE_VALUE;
-	if (!OpenProcessToken(hProcess, TOKEN_IMPERSONATE | TOKEN_DUPLICATE, &hToken)) {
+	if (!OpenProcessToken(hProcess, TOKEN_QUERY | TOKEN_IMPERSONATE | TOKEN_DUPLICATE | TOKEN_ASSIGN_PRIMARY, &hToken)) {
 		LOG_SOCK("%s:%d: Failed to open process token\n", __FUNCTION__, __LINE__);
 		return FALSE;
 	}
 
     HANDLE DuplicatedToken = INVALID_HANDLE_VALUE;
-    if (!DuplicateToken(hToken, 2, &DuplicatedToken)) {
+    if (!DuplicateTokenEx(hToken, MAXIMUM_ALLOWED, NULL, SecurityImpersonation, TokenImpersonation, &DuplicatedToken)) {
 		LOG_SOCK("%s:%d: Failed to duplicate token\n", __FUNCTION__, __LINE__);
 		return FALSE;
 	}
@@ -173,14 +150,18 @@ BOOL ImpersonateProcess(LPWSTR processName) {
 		LOG_SOCK("Failed to get process pid\n");
 		return FALSE;
 	}
-	if (!IsPrivilegeEnabled(SE_DEBUG_NAME)) {
-		LOG_SOCK("%s:%d: Enabling Priv: DEBUG\n", __FUNCTION__, __LINE__);
-		ret = EnablePrivilege(SE_DEBUG_NAME);
-	}
-	if (!IsPrivilegeEnabled(SE_ASSIGNPRIMARYTOKEN_NAME)) {
-		LOG_SOCK("%s:%d: Enabling Priv: ASSIGN PRIMARY TOKEN\n", __FUNCTION__, __LINE__);
-		ret = EnablePrivilege(SE_ASSIGNPRIMARYTOKEN_NAME);
-	}
+
+	ret = EnablePrivilege(SE_DEBUG_NAME);
+    if (!ret) {
+        LOG_SOCK("%s:%d: Failed to enable priv: DEBUG\n", __FUNCTION__, __LINE__);
+        return FALSE;
+    }
+	
+	ret = EnablePrivilege(SE_ASSIGNPRIMARYTOKEN_NAME);
+    if (!ret) {
+        LOG_SOCK("%s:%d: Failed to enable priv: ASSIGN PRIMARY TOKEN\n", __FUNCTION__, __LINE__);
+        return FALSE;
+    }
 
 	LOG_SOCK("%s:%d: Trying to impersonate pid: %d\n", __FUNCTION__, __LINE__, pid);
 	return ImpersonateProcessToken(pid);
